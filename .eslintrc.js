36e77dee10dcf07651d2778c92451ba6d236d1c0
TamaraHeater <tamaraheater@gmail.com>
2026-03-02 17:34:10 -0600

    eslint file and edits with rules

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const _id ="";
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+
+  // Add the necessary extensions.
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": ["warn", { "allow": ["error"] }] },
+    "no-underscore-dangle": ["error", { "allow": ["_id"] }]
+  },
+};
+
+
