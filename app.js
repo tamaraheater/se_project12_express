908ce5451b2226d5934636d7143a5f6c8c5bd60e
TamaraHeater <tamaraheater@gmail.com>
2026-03-23 10:03:18 -0500

    updates and error edits

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const { PORT = 3001 } = process.env;
 const indexRouter = require("./routes/index");
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").then(() => {
-  console.log("Connected to DB");
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db")
+.then(() => {
 });
 
 app.get("/", (req, res) => {
