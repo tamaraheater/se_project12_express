84d7d5af010b6a4c1d02d509929db6f33743b97d
TamaraHeater <tamaraheater@gmail.com>
2026-03-24 10:11:34 -0500

    testing working copy to commit process name change

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -6,7 +6,7 @@ const clothingItemSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 2,
-    maxlength: 30,
+    maxlength: 28,
   },
   weather: {
     type: String,
