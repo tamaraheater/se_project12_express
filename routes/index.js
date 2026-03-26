2cf6c393f4d091bcaeba72f6383763eabe17c285
TamaraHeater <tamaraheater@gmail.com>
2026-03-26 05:50:44 -0500

    corrected error message import

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = require("express").Router();
 const clothingItemsRouter = require("./clothingItems");
 const usersRouter = require("./users");
 
+const { NOT_FOUND } = require("../utils/errors");
+
 router.use("/users", usersRouter);
 router.use("/items", clothingItemsRouter);
 
