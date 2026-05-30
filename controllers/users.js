f9523df4fbf633351dd6538cf691b5f9947afe20
TamaraHeater <tamaraheater@gmail.com>
2026-05-30 01:23:34 -0500

    this was success at 5439m but never was sent to my email.

--- controllers/users.js
+++ controllers/users.js
@@ -1,128 +1,78 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
-const { JWT_SECRET } = require("../utils/config");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const {
-  BAD_REQUEST,
-  CONFLICT,
-  SERVER_ERROR,
-  UNAUTHORIZED,
-  NOT_FOUND,
-} = require("../utils/errors");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: [true, "The avatar field is required."],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL.",
+    },
+  },
+  email: {
+    type: String,
+    required: [true, "The email field is required."],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "You must enter a valid email.",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "The password field is required."],
+    minlength: 8,
+    select: false,
+  },
+});
 
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch((err) => {
-      console.error(err);
-      res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server." });
-    });
-};
-
-const createUser = (req, res) => {
-  const { name, avatar, email, password } = req.body;
-
-  User.create({ name, avatar, email, password })
-    .then((user) => {
-      const userWithoutPassword = {
-        _id: user._id,
-        name: user.name,
-        avatar: user.avatar,
-        email: user.email,
-      };
-      res.status(201).send(userWithoutPassword);
-    })
-    .catch((err) => {
-      console.error(err);
-
-      if (err.code === 11000) {
-        return res
-          .status(CONFLICT)
-          .send({ message: "A user with this email already exists" });
-      }
-
-      if (err.name === "ValidationError") {
-        return res
-          .status(BAD_REQUEST)
-          .send({ message: "Invalid data provided" });
-      }
-
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server." });
-    });
-};
-
-const login = (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res
-      .status(BAD_REQUEST)
-      .send({ message: "Email and password are required" });
+// Password hashing middleware
+userSchema.pre("save", async function passwordHashing(next) {
+  if (!this.isModified("password")) {
+    return next();
   }
 
-  return User.findUserByCredentials(email, password)
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email })
+    .select("+password")
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-        expiresIn: "7d",
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+        return user;
       });
-      res.send({ token });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(UNAUTHORIZED).send({ message: "Incorrect email or password" });
     });
 };
 
-const getCurrentUser = (req, res) => {
-  User.findById(req.user._id)
-    .orFail()
-    .then((user) => res.send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "CastError" || err.name === "DocumentNotFoundError") {
-        return res.status(NOT_FOUND).send({ message: "User not found" });
-      }
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server." });
-    });
-};
-
-const updateProfile = (req, res) => {
-  const { name, avatar } = req.body;
-
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name, avatar },
-    { new: true, runValidators: true }
-  )
-    .orFail()
-    .then((user) => res.send(user))
-    .catch((err) => {
-      console.error(err);
-
-      if (err.name === "ValidationError") {
-        return res
-          .status(BAD_REQUEST)
-          .send({ message: "Invalid data provided for update" });
-      }
-      if (err.name === "DocumentNotFoundError") {
-        return res.status(NOT_FOUND).send({ message: "User not found" });
-      }
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server." });
-    });
-};
-
-module.exports = {
-  getUsers,
-  createUser,
-  login,
-  getCurrentUser,
-  updateProfile,
-};
+module.exports = mongoose.model("user", userSchema);
